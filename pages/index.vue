3fda3658b1c20c9576bc293983fd8e315781a826
Ebihara Yusuke <ebiyuu1121@gmail.com>
2020-04-10 11:30:56 +0900

    [add] とりあえずsocketで接続できるようになった

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,70 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-row>
-      <v-col>
-        <v-btn @click="setProblem">現在の盤面を問題として設定</v-btn>
-      </v-col>
-      <v-col>
-        <v-btn color="primary" @click="clearAns">解答を削除</v-btn>
-      </v-col>
-      <v-col>
-        <v-btn color="error" @click="clearAll">全削除</v-btn>
-      </v-col>
-    </v-row>
-    <div class="cell-wrap">
-      <div class="cell-group__row" v-for="startRow in [0,3,6]" :key="startRow">
-        <div class="cell-group" v-for="startCol in [0,3,6]" :key="startCol">
-          <div class="cell__row" v-for="row in [startRow,startRow+1,startRow+2]" dense :key="row">
-            <div class="cell" v-for="col in [startCol,startCol+1,startCol+2]" :key="col">
-              <input
-                type="text"
-                class="num-cell"
-                :class="{
-                  highlighted: cellsHighlited[row][col],
-                  active: activeCell && row == activeCell.row && col == activeCell.col,
-                  problem: cells[row][col].isProblem
+  <v-layout>
+    <v-layout column justify-center align-center>
+      <v-row v-if="connected">
+        <span :style="`color: ${this.color}`">{{name}}</span>
+      </v-row>
+      <v-row v-if="!connected">
+        <v-text-field label="名前を入力" v-model="name"></v-text-field>
+      </v-row>
+      <v-row v-if="!connected">
+        <v-color-picker hide-canvas v-model="color"></v-color-picker>
+      </v-row>
+      <v-row v-if="!connected">
+        <v-btn color="primary" @click="connect">接続</v-btn>
+      </v-row>
+      <v-row v-if="connected">
+        <v-btn color="error" @click="disconnect">切断</v-btn>
+      </v-row>
+      <v-row v-if="connected">
+        <v-col>
+          <v-btn @click="setProblem">現在の盤面を問題として設定</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" @click="clearAns">解答を削除</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="error" @click="clearAll">全削除</v-btn>
+        </v-col>
+      </v-row>
+      <div class="cell-wrap" v-if="connected">
+        <div class="cell-group__row" v-for="startRow in [0,3,6]" :key="startRow">
+          <div class="cell-group" v-for="startCol in [0,3,6]" :key="startCol">
+            <div class="cell__row" v-for="row in [startRow,startRow+1,startRow+2]" dense :key="row">
+              <div class="cell" v-for="col in [startCol,startCol+1,startCol+2]" :key="col">
+                <input
+                  type="text"
+                  class="num-cell"
+                  :class="{
+                highlighted: cellsHighlited[row][col],
+                active: activeCell && row == activeCell.row && col == activeCell.col,
+                problem: cells[row][col].isProblem
                 }"
-                :readonly="cells[row][col].isProblem"
-                v-model="cells[row][col].value"
-                @focus="activeCell = {row: row, col: col}"
-              />
+                  :style="`color: ${cells[row][col].color}`"
+                  :readonly="cells[row][col].isProblem"
+                  v-model="cells[row][col].value"
+                  @focus="activeCell = {row: row, col: col}"
+                  @input="cellChanged"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </v-layout>
+    <v-layout>
+      <v-list v-if="connected">
+        <v-list-item v-for="(user, key) in users" :key="key">
+          <span :style="`color: ${user.color}`">{{user.name}}</span>
+        </v-list-item>
+      </v-list>
+    </v-layout>
   </v-layout>
 </template>
 
 <script>
+import io from "socket.io-client";
+
 export default {
   components: {},
   data() {
@@ -46,13 +74,19 @@ export default {
         JSON.stringify(
           new Array(9).fill(
             new Array(9).fill({
-              value: null,
+              value: "",
               isProblem: false,
-              inputBy: null
+              inputBy: "",
+              color: ""
             })
           )
         )
-      )
+      ),
+      socket: "",
+      name: "",
+      color: "",
+      connected: false,
+      users: {}
     };
   },
   computed: {
@@ -82,6 +116,7 @@ export default {
           }
         }
       }
+      this.socket.emit("cell-changed", this.cells);
     },
     clearAns() {
       for (let row = 0; row < 9; row++) {
@@ -91,34 +126,64 @@ export default {
           }
         }
       }
+      this.socket.emit("cell-changed", this.cells);
     },
     clearAll() {
       this.cells = JSON.parse(
         JSON.stringify(
           new Array(9).fill(
             new Array(9).fill({
-              value: null,
+              value: "",
               isProblem: false,
-              inputBy: null
+              inputBy: "",
+              color: ""
             })
           )
         )
       );
-    }
-  },
-  watch: {
-    cells: {
-      handler() {
-        [0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(row => {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(col => {
-            let val = this.cells[row][col].value;
-            if (val != "" && (isNaN(val) || val < 1 || val > 9)) {
-              this.$set(this.cells[row][col], "value", "");
+      this.socket.emit("cell-changed", this.cells);
+    },
+    cellChanged() {
+      [0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(row => {
+        [0, 1, 2, 3, 4, 5, 6, 7, 8].forEach(col => {
+          let val = this.cells[row][col].value;
+          if (val == "") {
+            if (this.cells[row][col].inputBy != "") {
+              this.$set(this.cells[row][col], "inputBy", "");
             }
-          });
+          } else {
+            if (isNaN(val) || val < 1 || val > 9) {
+              this.$set(this.cells[row][col], "value", "");
+            } else {
+              if (this.cells[row][col].inputBy == "") {
+                this.$set(this.cells[row][col], "inputBy", this.name);
+                this.$set(this.cells[row][col], "color", this.color);
+              }
+            }
+          }
         });
-      },
-      deep: true
+      });
+      this.socket.emit("cell-changed", this.cells);
+    },
+    connect() {
+      this.connected = true;
+
+      this.socket = io();
+      this.socket.emit("user-join", {
+        name: this.name,
+        color: this.color
+      });
+      this.socket.on("cell-changed", cells => {
+        this.cells = cells;
+      });
+      this.socket.on("user-update", users => {
+        this.users = users;
+      });
+    },
+    disconnect() {
+      this.socket.emit("user-leave", {});
+      this.socket.close();
+      this.connected = false;
     }
   }
 };
@@ -151,5 +216,5 @@ export default {
     border: red solid 2px
 
   &.problem
-    color: blue
+    color: black !important
 </style>
--- server/index.js
+++ server/index.js
@@ -1,33 +1,75 @@
-const express = require('express')
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const express = require('express');
+const consola = require('consola');
+const {Nuxt, Builder} = require('nuxt');
+const app = express();
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
-config.dev = process.env.NODE_ENV !== 'production'
+const config = require('../nuxt.config.js');
+config.dev = process.env.NODE_ENV !== 'production';
 
-async function start () {
-  // Init Nuxt.js
-  const nuxt = new Nuxt(config)
+async function start() {
+	// Init Nuxt.js
+	const nuxt = new Nuxt(config);
 
-  const { host, port } = nuxt.options.server
+	const {host, port} = nuxt.options.server;
 
-  await nuxt.ready()
-  // Build only in dev mode
-  if (config.dev) {
-    const builder = new Builder(nuxt)
-    await builder.build()
-  }
+	await nuxt.ready()
+	// Build only in dev mode
+	if (config.dev) {
+		const builder = new Builder(nuxt);
+		await builder.build();
+	}
 
-  // Give nuxt middleware to express
-  app.use(nuxt.render)
+	// Give nuxt middleware to express
+	app.use(nuxt.render);
 
-  // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
-  })
+	// Listen the server
+	let server = app.listen(port, host);
+	consola.ready({
+		message : `Server listening on http://${host}:${port}`,
+		badge : true
+	})
+
+	socketStart(server);
+	console.log('SocketIO.server starts');
 }
+
+let users = {};
+let cells = JSON.parse(
+    JSON.stringify(
+        new Array(9).fill(
+            new Array(9).fill({
+	            value : "",
+	            isProblem : false,
+	            inputBy : "",
+	            color : ""
+            }))));
+
+function socketStart(server) {
+	const io = require('socket.io').listen(server);
+
+	io.on('connection', socket => {
+		// 接続されたクライアントのidをコンソールに表示する
+		console.log('id: ' + socket.id + ' is connected');
+
+		// クライアントから送信があった場合のイベントを作成する
+		socket.on('user-join', user => {
+			users[socket.id] = user;
+			console.log(user.name + ' has joined');
+			io.emit('user-update', users);
+			io.emit('cell-changed', cells);
+		})
+
+		socket.on('cell-changed', newCells => {
+			cells = newCells;
+			socket.broadcast.emit('cell-changed', cells);
+		})
+
+		socket.on('user-leave', dic => {
+			delete users[socket.id];
+			socket.broadcast.emit('user-update', users);
+		})
+	})
+}
+
 start()
